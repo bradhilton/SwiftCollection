a0776b58d8f40df7604a4ec0bd8bc843ec3ceecf
bradhilton <brad.hilton.nw@gmail.com>
2016-04-11 14:10:36 -0600

    Initial Commit

---
+++ SwiftCollection/SwiftCollection.h
@@ -0,0 +1,19 @@
+//
+//  SwiftCollection.h
+//  SwiftCollection
+//
+//  Created by Bradley Hilton on 4/11/16.
+//  Copyright © 2016 Brad Hilton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftCollection.
+FOUNDATION_EXPORT double SwiftCollectionVersionNumber;
+
+//! Project version string for SwiftCollection.
+FOUNDATION_EXPORT const unsigned char SwiftCollectionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftCollection/PublicHeader.h>
+
+
